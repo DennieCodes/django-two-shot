09d000cc2485de40f5042757dd9a8a8c6c7fbbad
Dennie <oculareo@gmail.com>
2023-08-30 15:54:02 -0400

    Feature 8 complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from receipts.models import Receipt
 
 
 # LIST_RECEIPTS
+@login_required
 def home(request):
-    receipt_list = Receipt.objects.all()
+    receipt_list = Receipt.objects.filter(purchaser=request.user)
 
     context = {"receipt_list_object": receipt_list}
 
