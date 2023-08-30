f62d5ec6fff281b18d52101bd87c11c7bc2097e0
Dennie <oculareo@gmail.com>
2023-08-30 14:06:25 -0400

    Feature 5 complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from receipts.models import Receipt
 
-# Create your views here.
+
+# LIST_RECEIPTS
+def home(request):
+    receipt_list = Receipt.objects.all()
+
+    context = {"receipt_list_object": receipt_list}
+
+    return render(request, "receipts/list.html", context)
+
+    # todo_list = TodoList.objects.all()
+
+    # context = {"todo_list_object": todo_list}
+
+    # return render(request, "todos/list.html", context)
