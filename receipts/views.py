e957df1f119f952a37a26a3634a5909b1ee8f140
Dennie <oculareo@gmail.com>
2023-08-30 15:38:54 -0400

    Feature 7 complete

--- receipts/views.py
+++ receipts/views.py
@@ -9,9 +9,3 @@ def home(request):
     context = {"receipt_list_object": receipt_list}
 
     return render(request, "receipts/list.html", context)
-
-    # todo_list = TodoList.objects.all()
-
-    # context = {"todo_list_object": todo_list}
-
-    # return render(request, "todos/list.html", context)
